7fa8e4f76223decb98c7e809636767c71112b510
reawei <reawei@vip.qq.com>
2018-06-12 20:52:51 +0800

    update

--- reawei-server/src/main/java/com/reawei/server/commom/MybatisConfiguration.java
+++ reawei-server/src/main/java/com/reawei/server/commom/MybatisConfiguration.java
@@ -4,16 +4,12 @@ import com.reawei.server.commom.datasource.*;
 import org.apache.ibatis.plugin.Interceptor;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
